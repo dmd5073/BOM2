0b02e8157c645c001319a065be5bd92dde16c01f
Dustin Davis <dmd5073@gmail.com>
2013-03-23 18:36:34 -0500

    Update3-23-13

--- bomcreate.py
+++ bomcreate.py
@@ -14,11 +14,24 @@ youenter.youenter(Clientname, "Please Renter Your Client's Name:")
 ClientWON = raw_input ("Please Enter Your Client's Workorder Number:")
 youenter.youenter(ClientWON, "Please Renter Your Client's Workorder Number:")
 
+print "Now that you have provide the information require to setup the Bill of Material file."
+print "Please provide the following design parameters." 
+
+while True:
+    if (yesno.yesno("Will your design information change throughout this project(Y/N)?")):
+
+    else:
+        print "I Work"
+
+yesno.yesno("Will you have mutiple drawing that you will be adding to the Bill of Material(Y/N)?")
+
+DWG_NO = raw_input("Please Enter the Drawing Number where the Bill of Material item will be located:")
+youenter.youenter(DWG_NO, "Please Renter Your Drawing Number:")
+
 DP = raw_input ("Please Enter Your Design Pressure:")
 youenter.youenter(DP, "Please Renter Your Design Pressure:")
 
 DF = raw_input ("Please Enter Your Design Factor:")
 youenter.youenter(DF, "Please Renter Your Design Factor:")
 
-DWG_NO = raw_input("Please Enter the Drawing Number where the Bill of Material item will be located:")
-youenter.youenter(DWG_NO, "Please Renter Your Drawing Number:")
+print"Thank you for adding your design information."
