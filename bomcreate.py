1498f62e9672d61ae096b0048228382d81361e00
Dustin Davis <dmd5073@gmail.com>
2013-03-23 23:55:34 -0500

    update

--- bomcreate.py
+++ bomcreate.py
@@ -2,7 +2,7 @@ import csv
 import yesno
 import youenter
 
-if(not yesno.yesno("Welcome to the Bill of Material Creator. Would you like to continue (Y/N)?")):
+if(not yesno.yesno("Welcome to the Bill of Material Creator. Would you like to continue?(Y/N)")):
     exit()
 
 ProjectNum = raw_input ("Please Enter Your ENE Project Number:")
@@ -17,15 +17,26 @@ youenter.youenter(ClientWON, "Please Renter Your Client's Workorder Number:")
 print "Now that you have provide the information require to setup the Bill of Material file."
 print "Please provide the following design parameters." 
 
-while True:
-    if (yesno.yesno("Will your design information change throughout this project(Y/N)?")):
+while (yesno.yesno("Will you have mutiple drawing that you will be adding a Bill of Material too?(Y/N)")):
+    if (yesno.yesno("Will your design information change from drawing to drawing?(Y/N)")):
+        while (DWGNO = raw_input("Please Enter the Drawing Number where the Bill of Material item will be located:")):
+            youenter.youenter(DWG_NO, "Please Renter Your Drawing Number:")
+
+            DP = raw_input ("Please Enter Your Design Pressure:")
+            youenter.youenter(DP, "Please Renter Your Design Pressure:")
+
+            DF = raw_input ("Please Enter Your Design Factor:")
+            youenter.youenter(DF, "Please Renter Your Design Factor:")
+
+            yesno.yesno("Would you like to add another drawing?(Y/N")
         print "WES ADDED THIS LINE"
     else:
         print "I Work"
+        break
 
-yesno.yesno("Will you have mutiple drawing that you will be adding to the Bill of Material(Y/N)?")
 
-DWG_NO = raw_input("Please Enter the Drawing Number where the Bill of Material item will be located:")
+
+DWGNO = raw_input("Please Enter the Drawing Number where the Bill of Material item will be located:")
 youenter.youenter(DWG_NO, "Please Renter Your Drawing Number:")
 
 DP = raw_input ("Please Enter Your Design Pressure:")
